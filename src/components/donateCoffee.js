3ce5fcf29d2265fb7bcf017b3b1405be49904b92
Jerry <jerrylabs@seznam.cz>
2020-12-22 12:03:48 +0100

    Updates

--- src/components/donateCoffee.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default () => (
- <a className="bmc-button" target="_blank" rel="noopener noreferrer" href="https://www.buymeacoffee.com/JerryLabs">
-    <img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Přispět na kafe" />
-    <span>Přispět na kafe</span>
-  </a>
-)
