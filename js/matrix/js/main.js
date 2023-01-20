5f4a82deaed5893f6f917d9ac6b71276a4b086e2
Jaroslav Kameň <jaroslav.kamen@firma.seznam.cz>
2023-01-21 00:43:41 +0100

    Updates

---
+++ js/matrix/js/main.js
@@ -0,0 +1,45 @@
+import makeConfig from "/js/matrix/js/config.js";
+
+const canvas = document.createElement("canvas");
+document.querySelector('.jumbotron__bg').appendChild(canvas);
+document.addEventListener("touchmove", (e) => e.preventDefault(), {
+	passive: false,
+});
+
+const supportsWebGPU = async () => {
+	return window.GPUQueue != null && navigator.gpu != null && navigator.gpu.getPreferredCanvasFormat != null;
+};
+
+const isRunningSwiftShader = () => {
+	const gl = document.createElement("canvas").getContext("webgl");
+	const debugInfo = gl.getExtension("WEBGL_debug_renderer_info");
+	const renderer = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
+	return renderer.toLowerCase().includes("swiftshader");
+};
+
+	const config = makeConfig({skipIntro: 'false'});
+	const useWebGPU = (await supportsWebGPU()) && ["webgpu"].includes(config.renderer?.toLowerCase());
+	const solution = import(`/js/matrix/js/${useWebGPU ? "webgpu" : "regl"}/main.js`);
+
+	if (isRunningSwiftShader() && !config.suppressWarnings) {
+		const notice = document.createElement("notice");
+		notice.innerHTML = `<div class="notice">
+		<p>Wake up, Neo... you've got hardware acceleration disabled.</p>
+		<p>This project will still run, incredibly, but at a noticeably low framerate.</p>
+		<button class="blue pill">Plug me in</button>
+		<a class="red pill" target="_blank" href="https://www.google.com/search?q=chrome+enable+hardware+acceleration">Free me</a>
+		`;
+		canvas.style.display = "none";
+		document.body.appendChild(notice);
+		document.querySelector(".blue.pill").addEventListener("click", async () => {
+			config.suppressWarnings = true;
+			urlParams.set("suppressWarnings", true);
+			history.replaceState({}, "", "?" + unescape(urlParams.toString()));
+			(await solution).default(canvas, config);
+			canvas.style.display = "unset";
+			document.body.removeChild(notice);
+		});
+	} else {
+		(await solution).default(canvas, config);
+	}
+
